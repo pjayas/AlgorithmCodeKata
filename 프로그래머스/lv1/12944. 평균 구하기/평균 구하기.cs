3633556ff70c58cba3cc74980a206178d0c319ca
pjayas <115007528+pjayas@users.noreply.github.com>
2023-08-25 10:20:27 +0900

    [level 1] Title: 평균 구하기, Time: 0.25 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public double solution(int[] arr) {
+        double sum = 0;
+        foreach (int num in arr) {
+            sum += num;
+        }
+            return sum / arr.Length;
+    }
+}
