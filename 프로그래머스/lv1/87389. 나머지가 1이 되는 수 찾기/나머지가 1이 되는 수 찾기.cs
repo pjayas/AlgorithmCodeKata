ffe7d273b3de43dfa929709985ca716120c9a230
pjayas <115007528+pjayas@users.noreply.github.com>
2023-08-29 09:13:12 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.15 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        for(int x = 2;x<=n;x++)
+        {
+            if(n%x==1)
+            {
+                return x;
+            }
+        }
+        return n;
+    }
+}
